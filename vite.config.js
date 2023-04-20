427646137b83e1f96ae6138a61120bb9971219ad
colecrum26 <cwcrumrine@gmail.com>
2023-04-20 13:48:03 -0500

    updated base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/activity-generator",
+  base: "/activity-generator/",
   plugins: [react()],
 })
